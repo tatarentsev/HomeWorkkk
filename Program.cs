90291b1877c7ceabb6f9686fec36e6fe65735fbb
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-03-02 00:07:17 +0300

    Write code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿string[] strings = {"hello", "2", "world", ":)"}; // 4
+
+int count = 0; // 2
+for (int j = 0; j < strings.Length; j++) // 0 < 3
+{
+    if (strings[j].Length <= strings.Length + 1)
+    {
+        count++; // 2
+    }
+}
+
+
+string[] copy = new string[count];
+for (int i = 0; i < copy.Length; i++) // 0 < 2
+{
+    if (strings[i].Length <= 3)
+    {
+        copy[i] = strings[i];
+    }
+}
+
+Console.Write("[");
+for (int k = 0; k < count; k++)
+{
+    Console.Write($"{copy[k]} ");
+}
+Console.Write("]");
